c46888ddeca903b739f5818bd4f3d5a56498590d
msobolak <marek.sobolak@gmail.com>
2022-10-29 12:09:03 +0200

    Create algoprice.py

---
+++ algoprice.py
@@ -0,0 +1,26 @@
+import json
+import sys
+import os
+
+datafile = sys.argv[1]
+asset1name = os.environ["ASSET1"]
+asset2name = os.environ["ASSET2"]
+asset1price = ""
+asset2price = ""
+
+with open(datafile, "r") as fd:
+    data = json.load(fd)
+
+for asset_ in data:
+  if asset_['name'] == asset1name:
+      asset1price = float(asset_['price']))
+  if asset_['name'] == asset2name:
+      asset2price = float(asset_['price']))
+
+if no asset1price:
+    raise IOError(f"Cannot find price of {asset1name}.")
+
+if no asset2price:
+    raise IOError(f"Cannot find price of {asset2name}.")
+
+print(asset2price / asset1price)
